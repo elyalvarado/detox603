d9b932539117d888823795ee6c5899b1c766f3a1
elyalvarado <elyalvarado@gmail.com>
2019-07-17 16:29:04 -0500

    Failing Accesibility Label test on Android

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
   ScrollView,
   View,
   Text,
+  TouchableOpacity,
   StatusBar,
 } from 'react-native';
 
@@ -39,32 +40,26 @@ const App = () => {
             </View>
           )}
           <View style={styles.body}>
-            <View style={styles.sectionContainer}>
+            <View
+              accessible={true}
+              accessibilityLabel="Edit App.js Container"
+              style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
+            <TouchableOpacity
+              accessibilityLabel="See your changes button"
+              style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
+            </TouchableOpacity>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
+              <Text testID="DebugText" style={styles.sectionTitle}>
+                Debug
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -7,13 +7,15 @@ describe('Example', () => {
     await expect(element(by.text('Welcome to React'))).toBeVisible();
   });
 
-  xit('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it('should find an element by testID', async () => {
+    await expect(element(by.id('DebugText'))).toBeVisible();
   });
 
-  xit('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should find a view by accesibility label', async () => {
+    await expect(element(by.label('Edit App.js Container'))).toBeVisible();
+  });
+
+  it('should find a button by accesibility label', async () => {
+    await expect(element(by.label('See your changes button'))).toBeVisible();
   });
 });
